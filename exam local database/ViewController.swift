d1c4a78a41d5ab378f4a96789aab964599a49bc3
monil sojitra <monilsojitra42@gmail.com>
2023-04-12 11:08:08 +0530

    Initial Commit

---
+++ exam local database/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exam local database
+//
+//  Created by monil sojitra on 12/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
